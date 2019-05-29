a983e4d09ff3e7a7a2b17315d6a9c9db0ef96b2f
lao <498690132@qq.com>
2019-05-29 20:54:19 +0800

    Initial commit

---
+++ app/src/main/java/com/glut/shop/activity/UserActivity.java
@@ -0,0 +1,29 @@
+package com.glut.shop.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.glut.shop.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        // 从布局文件中获取名叫tl_head的工具栏
+        Toolbar tl_head = findViewById(R.id.tl_head);
+        // 使用tl_head替换系统自带的ActionBar
+        setSupportActionBar(tl_head);
+        // 给tl_head设置导航图标的点击监听器
+        // setNavigationOnClickListener必须放到setSupportActionBar之后，不然不起作用
+        tl_head.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
